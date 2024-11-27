d4855aed25899a56dd435bdeb8d8727adefc34a2
musfirah-mohd-ali <s10270156@connect.np.edu.sg>
2024-11-27 14:03:24 +0800

    added data for the tables

---
+++ transaction/scriptTransaction.js
@@ -0,0 +1,34 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const transactionData = [
+        {
+            bookingType: "Driving Lesson",
+            date: "2024-11-04",
+            timeSlot: "9:00 AM - 10:40 AM",
+            lessonType: "Practical",
+            totalPrice: "$89.75"
+        },
+        {
+            bookingType: "Theory Lesson",
+            date: "2024-11-12",
+            timeSlot: "8:30 AM - 9:30 AM",
+            lessonType: "Theory",
+            totalPrice: "$45.60"
+        },
+        {
+            bookingType: "Driving Lesson",
+            date: "2024-11-14",
+            timeSlot: "2:30 PM - 5:20 PM",
+            lessonType: "Practical",
+            totalPrice: "$89.75"
+        },
+        {
+            bookingType: "Circuit Improvement",
+            date: "2024-11-30",
+            timeSlot: "2:30 PM - 5:20 PM",
+            lessonType: "Practical",
+            totalPrice: "$89.75"
+        },
+    ];
+
+}    
+)
